dce177b62d3263d3b2e029d4313098fc06111dd5
geersann <148795588+geersann@users.noreply.github.com>
2024-08-09 14:50:37 +0000

    add items, and render in li

--- src/App.js
+++ src/App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import './App.css';
 
+const items = [
+  {id: 1, name: 'Animals'},
+  {id: 2, name: 'Anime'},
+  {id: 3, name: 'Anti-Malware'},
+  {id: 4, name: 'Art Design'},
+  {id: 5, name: 'Books'},
+  {id: 6, name: 'Business'},
+  {id: 7, name: 'Calendar'},
+  {id: 8, name: 'Cloud Storage'},
+  {id: 9, name: 'File Sharing'},
+  {id: 10, name: 'Animals'},
+  {id: 11, name: 'Continuous Integration'},
+  {id: 12, name: 'Cryptocurrency'}
+]
+
 export default function App() {
   return (
     <div>
-      Some List:
+      <h3>Some List:</h3>
+      <ul>
+        {items.map(item => (
+          <li key={item.id}>{item.name}</li>
+        ))}
+      </ul>
     </div>
   );
-}
+};
